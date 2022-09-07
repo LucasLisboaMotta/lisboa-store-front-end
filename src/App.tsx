d19cfd4fdfd1a6b6f2599a630c606db24cf4f270
Lucas Lisboa <lucaslisboamotta@gmail.com>
2022-09-07 19:31:20 -0300

    feat: Implementando componentes criados no app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import ProductProvider from "./context/ProductProvider";
+import CreateCard from "./components/CreateCard";
+import Header from "./components/Header";
+import ListProductCard from "./components/ListProductCard";
+import ProductCardDetail from "./components/ProductCardDetail";
+import ProductContext from "./context/ProductContext";
+import { useContext } from "react";
 
 function App() {
+  const { isEditMode, isCreateMode } = useContext(ProductContext);
+  const startCreateCard = isEditMode || isCreateMode;
+
   return (
-    <ProductProvider>
       <div className="App">
+        <Header />
+        {startCreateCard && <CreateCard />}
+        <ListProductCard />
+        <ProductCardDetail />
       </div>
-    </ProductProvider>
   );
 }
 
