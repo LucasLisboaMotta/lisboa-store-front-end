39f7600d087edbcd6dc4fd34c5597841e29c06df
Lucas Lisboa <lucaslisboamotta@gmail.com>
2022-09-08 01:40:39 -0300

    style: Renomeando arquivo

--- src/styles/headerStyle.ts
+++ src/styles/headerStyle.ts
@@ -6,37 +6,37 @@ const h1Styles: CSS.Properties = {
 
 const searchIconStyles: CSS.Properties = {
   width: '20px',
-}
+};
 
 const headerStyle: CSS.Properties = {
   backgroundColor: 'rgb(50, 167, 145)',
-}
+};
 
 const searchInputStyle: CSS.Properties = {
   fontSize: '20px',
   paddingTop: '2px',
-}
+};
 
 const addDivStyle: CSS.Properties = {
   display: 'flex',
   justifyContent: 'flex-end',
-}
+};
 
 const addSpanStyle: CSS.Properties = {
   marginRight: '5px',
   marginBottom: '5px',
   color: 'rgb(0, 0, 0)',
-}
+};
 
 const addButtonStyle: CSS.Properties = {
   width: '20px',
   backgroundColor: '#666666',
   borderRadius: '25%',
-}
+};
 
 const plusIconStyle: CSS.Properties = {
   width: '10px',
-}
+};
 
 export {
   plusIconStyle,
@@ -47,4 +47,4 @@ export {
   headerStyle,
   searchInputStyle,
   addDivStyle,
-}
+};
